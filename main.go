b2af97dbd5db25165b5c4b11a375f46e6e36240f
Alexander Blair <git@jagtech.io>
2025-05-22 22:35:29 -0700

    fix(main): add powdata
    Set the pow data equal to byte 47+

--- main.go
+++ main.go
@@ -262,6 +262,7 @@ func handleSubmitBlock(c *gin.Context, bodyAsByteArray []byte) {
 	if v, ok := tariBlockCache[fmt.Sprintf("%x", mmHash)]; ok {
 		blockData := v.Block
 		blockData.Header.Nonce = uint64(binary.BigEndian.Uint32(rawTariBt[39:43]))
+		blockData.Header.Pow.PowData = rawTariBt[47:]
 		if blockResp, err := nodeGRPC.SubmitBlock(blockData); err != nil {
 			milieu.CaptureException(err)
 			c.JSON(400, rpcResultError{
